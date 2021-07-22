a753a58161300581d71906c8baa889c5114c53bd
guoruize <13331135676@163.com>
2021-07-22 16:15:25 +0800

    Create a new iOS Project!!!

---
+++ Moments/Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by guoruize on 2021/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

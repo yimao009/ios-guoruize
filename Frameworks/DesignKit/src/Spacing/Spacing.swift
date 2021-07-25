91022f53cca1f65f7f03b6b2c7763eaac7164ec0
guoruize <13331135676@163.com>
2021-07-25 12:54:31 +0800

    Add `Spacing` to DesignKit

---
+++ Frameworks/DesignKit/src/Spacing/Spacing.swift
@@ -0,0 +1,19 @@
+//
+//  Spacing.swift
+//  DesignKit
+//
+//  Created by guoruize on 2021/7/23.
+//
+
+import UIKit
+
+public struct Spacing {
+    public static let twoExtraSmall: CGFloat = 4
+    public static let extraSmall: CGFloat = 8
+    public static let small: CGFloat = 12
+    public static let medium: CGFloat = 18
+    public static let large: CGFloat = 24
+    public static let extraLarge: CGFloat = 32
+    public static let twoExtraLarge: CGFloat = 40
+    public static let threeExtraLarge: CGFloat = 48
+}

37955d4d37e56d2326e084c2864889abaaef825e
guoruize <13331135676@163.com>
2021-07-26 12:18:06 +0800

    Add `MomentsRepo`

---
+++ Moments/Moments/Features/Moments/Repositories/MomentsRepo.swift
@@ -0,0 +1,33 @@
+//
+//  MomentsRepo.swift
+//  Moments
+//
+//  Created by guoruize on 2021/7/26.
+//
+
+import Foundation
+import RxSwift
+
+protocol MomentsRepoType {
+    func getMoments(userID: String) -> Observable<MomentsDetails>
+}
+
+class MomentsRepo: MomentsRepoType {
+
+    private let getMomentsByUserIDAPISessionBuilder: () -> GetMomentsByUserIDAPISessonType
+
+    static var shared: MomentsRepo = {
+        MomentsRepo {
+            GetMomentsByUserIDAPISession()
+        }
+    }()
+
+    init(getMomentsByUserIDAPISessionBuilder: @escaping () -> GetMomentsByUserIDAPISessonType) {
+        self.getMomentsByUserIDAPISessionBuilder = getMomentsByUserIDAPISessionBuilder
+    }
+
+    func getMoments(userID: String) -> Observable<MomentsDetails> {
+        getMomentsByUserIDAPISessionBuilder().getMoments(userID: userID)
+    }
+
+}

d173363cefce852ece10d2f1d2ad80407f38366c
guoruize <13331135676@163.com>
2021-07-27 10:50:54 +0800

    Nest the models 把模型嵌套到一个文件中

--- Moments/Moments/Features/Moments/Models/MomentsDetails.swift
+++ Moments/Moments/Features/Moments/Models/MomentsDetails.swift
@@ -10,4 +10,33 @@ import Foundation
 struct MomentsDetails: Codable {
     let userDetails: UserDetails
     let moments: [Moment]
+
+    struct UserDetails: Codable {
+        let id: String
+        let name: String
+        let avatar: String
+        let backgroundImage: String
+    }
+
+    struct Moment: Codable {
+
+        struct MomentUserDetails: Codable {
+            let name: String
+            let avatar: String
+        }
+
+        // swiftlint:disable no_hardcoded_strings
+        enum MomentType: String, Codable {
+            case url = "URL"
+            case photos = "PHOTOS"
+        }
+
+        let id: String
+        let userDetails: MomentUserDetails
+        let type: MomentType
+        let title: String?
+        let url: String?
+        let photos: [String]
+        let createdDate: String
+    }
 }
